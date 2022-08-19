0760a135d0046e751bc146cd564febdd0cbff9c8
hefei.cao <hefei.cao@xiaoyangedu.com>
2022-08-19 13:21:12 +0800

    init

---
+++ src/completionAt.ts
@@ -0,0 +1,81 @@
+import * as vscode from "vscode";
+import utils from "./utils";
+const getColor = require("get-css-colors");
+
+function provideCompletionItems(
+  document: vscode.TextDocument,
+  position: vscode.Position
+) {
+  // 光标位置不是@不处理
+  if (document.lineAt(position).text[position.character - 1] !== "@") {
+    return;
+  }
+
+  // 文件路径
+  const allFile = utils.getLocations(document) || [];
+
+  // 汇总所有变量
+  const allVars = utils.getVarsByFiles(allFile);
+
+  const allDepVars = utils.getDepVars(allVars);
+
+  const total = [];
+  for (let key in allDepVars) {
+    const documentation = allDepVars[key].reduce((pre, value, index) => {
+      return (
+        pre +
+        `${value.key} : ${value.value} ;${
+          index < allDepVars[key].length - 1 ? "\n" : ""
+        }`
+      );
+    }, "");
+
+    const lastColor = getColor(
+      allDepVars[key][allDepVars[key].length - 1].value
+    );
+
+    if (lastColor && lastColor.length) {
+      total.push({
+        detail: lastColor[lastColor.length - 1],
+        label: key,
+        kind: vscode.CompletionItemKind.Color,
+        documentation,
+      });
+    } else {
+      total.push({
+        label: key,
+        kind: vscode.CompletionItemKind.Variable,
+        documentation,
+      });
+    }
+  }require("./setLocations")(context); // 设置路径的webview
+
+  return total.length
+    ? total
+    : [
+        {
+          label: "@less-vars",
+          kind: vscode.CompletionItemKind.Text,
+          documentation:
+            "未找到变量,可在setting.json中设置lessVars.locations为less文件绝对路径",
+        },
+      ];
+}
+
+function resolveCompletionItem() {
+  return null;
+}
+
+module.exports = function (context: vscode.ExtensionContext) {
+  // 注册代码建议提示，只有当按下“@”时才触发
+  context.subscriptions.push(
+    vscode.languages.registerCompletionItemProvider(
+     ['vue','less'],
+      {
+        provideCompletionItems,
+        resolveCompletionItem,
+      },
+      "@"
+    )
+  );
+};
---
+++ src/extension.ts
@@ -0,0 +1,31 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	require("./completionAt")(context); // 自动补全@
+
+	require("./setLocations")(context); // 设置路径的webview
+
+	
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "lessvar-chris" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('lessvar-chris', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from lessVar_chris!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
