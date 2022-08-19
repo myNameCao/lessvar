b8611333bcbd472698fd60726e9dc1dde428dbd3
hefei.cao <hefei.cao@xiaoyangedu.com>
2022-08-19 17:16:36 +0800

    fix: 可以设置文件路基

--- src/setLocations.ts
+++ src/setLocations.ts
@@ -1,16 +1,16 @@
-import * as vscode from "vscode";
-import utils from "./utils";
+import * as vscode from 'vscode'
+import utils from './utils'
 
-let currentPanel: vscode.WebviewPanel | undefined = undefined;
+let currentPanel: vscode.WebviewPanel | undefined = undefined
 
 function setWebview(
   currentPanel: vscode.WebviewPanel | undefined,
   newLocation?: string[]
 ) {
   if (!currentPanel) {
-    return;
+    return
   }
-  const locations = newLocation || utils.getLocations() || [];
+  const locations = newLocation || utils.getLocations() || []
   currentPanel.webview.html = `
     <script>const vscode = acquireVsCodeApi();</script>
     <button onClick="vscode.postMessage({id:'selectFile'});">添加文件</button>
@@ -21,118 +21,117 @@ function setWebview(
         (v, index) =>
           `<li>${v}<button onClick="vscode.postMessage({id:'up',index:${index}});">上移</button><button onClick="vscode.postMessage({id:'delete',index:${index}});">删除</button></li>`
       )
-      .join("")}</ul>
-  `;
+      .join('')}</ul>
+  `
 }
 
 function registerCommand(context: vscode.ExtensionContext) {
   return () => {
     const columnToShowIn = vscode.window.activeTextEditor
       ? vscode.window.activeTextEditor.viewColumn
-      : undefined;
+      : undefined
 
     if (currentPanel) {
       // If we already have a panel, show it in the target column
-      currentPanel.reveal(columnToShowIn);
+      currentPanel.reveal(columnToShowIn)
     } else {
       // Otherwise, create a new panel
       currentPanel = vscode.window.createWebviewPanel(
-        "setLocations",
-        "less vars",
+        'setLocations',
+        'chris 帮你设置路径',
         columnToShowIn ?? vscode.ViewColumn.One,
         { enableScripts: true }
-      );
+      )
 
-      setWebview(currentPanel);
+      setWebview(currentPanel)
 
       // Reset when the current panel is closed
       currentPanel.onDidDispose(
         () => {
-          currentPanel = undefined;
+          currentPanel = undefined
         },
         null,
         context.subscriptions
-      );
+      )
 
       currentPanel.onDidChangeViewState(
-        (e) => {
+        e => {
           if (e.webviewPanel.visible) {
-            setWebview(currentPanel);
+            setWebview(currentPanel)
           }
         },
         null,
         context.subscriptions
-      );
+      )
 
       currentPanel.webview.onDidReceiveMessage(
-        (message) => {
-          if (message.id === "openEdit") {
-            vscode.commands.executeCommand("workbench.action.openSettingsJson");
-          } else if (message.id === "selectFile") {
+        message => {
+          if (message.id === 'openEdit') {
+            vscode.commands.executeCommand('workbench.action.openSettingsJson')
+          } else if (message.id === 'selectFile') {
             vscode.window
               .showOpenDialog({
-                filters: { less: ["less"], javascript: ["js"] },
+                filters: { less: ['less'], javascript: ['js'] }
               })
-              .then((res) => {
+              .then(res => {
                 if (res && res.length) {
-                  const pre = utils.getLocations() || [];
-                  const newLocation = [...new Set([res[0].fsPath, ...pre])];
+                  const pre = utils.getLocations() || []
+                  const newLocation = [...new Set([res[0].fsPath, ...pre])]
+                  console.log(11)
                   vscode.workspace
                     .getConfiguration()
                     .update(
-                      "lessVars.locations",
+                      'lessvarChris.locations',
                       newLocation,
                       vscode.ConfigurationTarget.Global
                     )
                     .then(() => {
-                      setWebview(currentPanel, newLocation);
-                    });
+                      setWebview(currentPanel, newLocation)
+                    })
                 }
-              });
-          } else if (message.id === "delete") {
-            const newLocation = utils.getLocations() || [];
-            newLocation.splice(message.index, 1);
+              })
+          } else if (message.id === 'delete') {
+            const newLocation = utils.getLocations() || []
+            newLocation.splice(message.index, 1)
             vscode.workspace
               .getConfiguration()
               .update(
-                "lessVars.locations",
+                'lessvarChris.locations',
                 newLocation,
                 vscode.ConfigurationTarget.Global
               )
               .then(() => {
-                setWebview(currentPanel, newLocation);
-              });
-          } else if (message.id === "up") {
-            const newLocation = utils.getLocations() || [];
-            const item = newLocation.splice(message.index, 1)[0];
+                setWebview(currentPanel, newLocation)
+              })
+          } else if (message.id === 'up') {
+            const newLocation = utils.getLocations() || []
+            const item = newLocation.splice(message.index, 1)[0]
             newLocation.splice(
               message.index - 1 >= 0 ? message.index - 1 : newLocation.length,
               0,
               item
-            );
+            )
             vscode.workspace
               .getConfiguration()
               .update(
-                "lessVars.locations",
+                'lessvarChris.locations',
                 newLocation,
                 vscode.ConfigurationTarget.Global
               )
               .then(() => {
-                setWebview(currentPanel, newLocation);
-              });
+                setWebview(currentPanel, newLocation)
+              })
           }
         },
         undefined,
         context.subscriptions
-      );
+      )
     }
-  };
+  }
 }
 
-export default   (context: vscode.ExtensionContext)=> {
-
-console.log(111)
+export default (context: vscode.ExtensionContext) => {
   context.subscriptions.push(
-    vscode.commands.registerCommand("setLocations", registerCommand(context))
-  );
-};
+    vscode.commands.registerCommand('setLocations', registerCommand(context))
+  )
+}
